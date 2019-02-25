7e4e188af537a6e1741656eb3599bc184719120c
Biswas <734192@cognizant.com>
2019-02-25 13:39:24 +0530

    max depth of a tree

---
+++ Trees/max_depth.cpp
@@ -0,0 +1,15 @@
+/*
+    Find max depth
+    https://github.com/susantabiswas/interviewbit-solutions.git
+    TC: O(n)
+ */
+
+int Solution::maxDepth(TreeNode* root) {
+    if(!root)
+        return 0;
+        
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+    
+    return max(l, r) + 1;
+}
