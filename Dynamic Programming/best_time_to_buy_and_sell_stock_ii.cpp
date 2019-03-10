f47e358dfd760231d1591d66e57cca60281a7a04
Susanta Biswas <susantasky@gmail.com>
2019-03-10 23:19:50 +0530

    max profit when stocks can be bought and sold many times

--- Dynamic Programming/best_time_to_buy_and_sell_stock_ii.cpp
+++ Dynamic Programming/best_time_to_buy_and_sell_stock_ii.cpp
@@ -5,6 +5,9 @@
 */
 
 int Solution::maxProfit(const vector<int> &arr) {
+    if(arr.empty())
+           return 0;
+    
     int max_profit = 0;
     
     // whenever we get to buy a stock for cheap and sell for a higher 
