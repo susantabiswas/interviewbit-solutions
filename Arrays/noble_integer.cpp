5c3a84810bc5bde0a9b746aab583e8a8f09120d2
Susanta Biswas <susantasky@gmail.com>
2019-03-14 17:54:22 +0530

    Create noble_integer.cpp

---
+++ Arrays/noble_integer.cpp
@@ -0,0 +1,29 @@
+/*
+    Given an integer array, find if an integer number exists in the array such that the 
+    number of integers greater than that in the array equals to it
+    https://www.interviewbit.com/problems/noble-integer/
+*/
+
+int Solution::solve(vector<int> &arr) {
+    sort(arr.begin(), arr.end());
+    
+    int i = arr.size() - 1;
+    const int N = arr.size();
+    while(i >= 0) {
+        // find the number of greater numbers
+        int greater_left = N - i - 1;
+        // if condition is met
+        if(arr[i] == greater_left)
+            return 1;
+        
+        // now we need to go to next unique number
+        int curr = arr[i];
+        --i;
+        // traverse till a different number is reached, this is
+        // done due to duplicates
+        while(i >= 0 && arr[i] == curr)
+            --i;
+    }
+    
+    return -1;
+}
