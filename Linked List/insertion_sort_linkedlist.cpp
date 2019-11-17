ec97b19b688f434a8b1bd42021938fa730522833
Susanta Biswas <susantasky@gmail.com>
2019-11-17 17:58:06 +0530

    Update insertion_sort_linkedlist.cpp

--- Linked List/insertion_sort_linkedlist.cpp
+++ Linked List/insertion_sort_linkedlist.cpp
@@ -4,7 +4,57 @@
 
     TC: O(n)
 */
+/*
+    Solution 1: Nodes are swapped during sorting
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+// Inserts the node in its correct position to 
+// keep the list still sorted
+void insertSortedList(ListNode *&head, ListNode* node) {
+    ListNode *prev = nullptr, *iter = head;
+    while(iter) {
+        // current node needs to be inserted here
+        if(iter->val > node->val) 
+            break;
+        prev = iter;
+        iter = iter->next;
+    }
+    prev->next = node;
+    node->next = iter;
+}
 
+ListNode* Solution::insertionSortList(ListNode* head) {
+    // head of sorted list
+    ListNode *sorted_head = new ListNode(INT_MIN), *sorted_tail = nullptr;
+    ListNode *iter = head, *prev = nullptr, *nextptr = nullptr;
+    
+    // traverse the nodes in list and put the
+    // node in the sorted list created so far
+    while(iter) {
+        nextptr = iter->next;
+        insertSortedList(sorted_head, iter);
+        iter = nextptr;
+    }
+    // delete the dummy node
+    ListNode* temp = sorted_head;
+    sorted_head = sorted_head->next;
+    delete temp;
+    
+    return sorted_head;
+}
+
+
+/*
+    Solution 2: Only node values are swapped during sorting and not the nodes
+*/
 ListNode* Solution::insertionSortList(ListNode* head) {
     ListNode* curr = head, *iter = nullptr;
     
