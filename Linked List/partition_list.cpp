1b41e39dea121df9dd564246dde50330c988cf09
Susanta Biswas <susantasky@gmail.com>
2019-11-17 17:08:01 +0530

    Update partition_list.cpp

--- Linked List/partition_list.cpp
+++ Linked List/partition_list.cpp
@@ -2,55 +2,45 @@
     Partiton linked list acc. to pivot
     https://www.interviewbit.com/problems/partition-list/
     TC: O(n) 
-*/
+*/ 
+// Inserts the node in the passed list
+void insertNode(ListNode*& head, ListNode*& tail, ListNode* node) {
+    // first node of the list
+    if(!head) {
+        head = node;
+        tail = node;
+        return; 
+    }
+    
+    // append the new node at the back 
+    tail->next = node;
+    // update the tail pointer and make its next null
+    tail = tail->next;
+    tail->next = nullptr;
+}
 
 ListNode* Solution::partition(ListNode* head, int pivot) {
-    if(!head)
-        return nullptr;
-        
-    ListNode* iter = head;
-    // we will maintain two lists: smaller and greater(contains equal too)
-    ListNode* smaller = nullptr, *greater = nullptr;
-    ListNode* tail_smaller = nullptr, *tail_greater = nullptr;
-  while(iter) {
-        // when data is lesser than pivot
-        if(iter->val < pivot) {
-            if(smaller == nullptr) {
-                smaller = iter;
-                tail_smaller = smaller;
-            }
-            else {
-                tail_smaller->next = iter;
-                tail_smaller = tail_smaller->next;
-            }
-        }
-       else {   // current data >= pivot 
-            if(greater == nullptr) {
-                greater = iter;
-                tail_greater = greater;
-            }
-            else {
-                tail_greater->next = iter;
-                tail_greater = tail_greater->next;
-            }
-        }
-        // traverse to next node
-        iter = iter->next;
-    }
-  
-    // end the three lists
-    if(tail_smaller)
-        tail_smaller->next = nullptr;
-    if(tail_greater)
-        tail_greater->next = nullptr;
-        
-   // choose a suitable head for partitioned list
-    ListNode* partitioned_head = smaller ? smaller : 
-                                    ( greater ? greater : nullptr);
+    ListNode *iter = head, *nextptr = nullptr;
+    // heads and tails of lesser and greater nodes lists wrt pivot
+    ListNode *lesser_head = nullptr, *lesser_tail = nullptr;
+    ListNode *greater_head = nullptr, *greater_tail = nullptr;
     
-    // now append the two lists: smaller->greater
-    if(smaller) 
-        tail_smaller->next = greater ? greater : nullptr;
-        
-    return partitioned_head;
+    // traverse the list and based on the value of each node
+    // append them to lesser or greater side
+    while(iter) {
+        nextptr = iter->next;
+        if(iter->val < pivot)
+            insertNode(lesser_head, lesser_tail, iter);
+        else 
+            insertNode(greater_head, greater_tail, iter);
+            
+        iter = nextptr;
+    }
+    
+    // join the smaller and larger nodes list
+    if(lesser_head) 
+        lesser_tail->next = greater_head;
+    
+    // if there are no lesser nodes list, then return the greater nodes list
+    return lesser_head ? lesser_head : greater_head;
 }
