fbcd651f090ca783c1b6cba2515cb1a308e14006
Susanta Biswas <susantasky@gmail.com>
2019-12-08 20:40:47 +0530

    Update single_number_ii.cpp

--- Bit Manipulation/single_number_ii.cpp
+++ Bit Manipulation/single_number_ii.cpp
@@ -4,6 +4,36 @@
 */
 
 /*
+    Without using extra space.
+    TC: O(n), n: no. of bits
+    SC: O(1), n: no. of bits
+*/
+int Solution::singleNumber(const vector<int> &arr) {
+    // acts as mask and also as a loop cond
+    // for no. of bits
+    unsigned int mask = 1;
+    int num = 0;
+    
+    while(mask) {
+        // for the current bit position, find
+        // no. of set bits in array elements
+        int ones = 0;
+        for(const int& el: arr)
+            if(el & mask)
+                ++ones;
+        // decide the bit value in resultant number
+        // and using the mask set the bit 
+        num = (ones % 3 != 0) ? (num | mask) : num;
+        mask = mask << 1;
+    }
+    
+    return num;
+}
+
+
+/*
+    TC: O(n), n: no. of bits
+    SC: O(n), n: no. of bits
     Since each number except one is repeate d3 times, so their contribution for
     each digit is also divisble by 3. So if for ith position, required number has:
     1: there will be 3x + 1 ones at ith position
