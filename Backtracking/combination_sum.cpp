ab4c5f76390df4fadbc2742cd3b0492aa1bebc54
Susanta Biswas <susantasky@gmail.com>
2020-03-15 18:10:00 +0530

    Update combination_sum.cpp

--- Backtracking/combination_sum.cpp
+++ Backtracking/combination_sum.cpp
@@ -3,6 +3,51 @@
     https://www.interviewbit.com/problems/combination-sum/
 */
 
+// SOLUTION 1
+/*
+    https://www.interviewbit.com/problems/combination-sum/
+    TC: O(2^N)
+*/
+
+void findCmbSum(int curr, vector<int>& arr,
+                int sum, vector<int> partial,
+                vector<vector<int>>& result) {
+    
+    // when sum if achieved or all the numbers have been traversed
+    if(sum <= 0 || curr == arr.size()) {
+        if(sum == 0) {
+            result.emplace_back(move(partial));
+        }
+        return;
+    }
+    
+    for(int i = curr; i < arr.size(); i++) {
+        partial.emplace_back(arr[i]);
+        // for the next recursive call, the sent index is the current one
+        // so that we have the choice of using or not using it
+        findCmbSum(i, arr, sum - arr[i], partial, result);
+        partial.pop_back();
+    }
+}
+
+vector<vector<int> > Solution::combinationSum(vector<int> &arr, int t) {
+    vector<vector<int>> result;
+    vector<int> partial;
+    vector<int> unique_nums;
+    sort(arr.begin(), arr.end());
+    
+    // since a number can be used multiple times anyways, this makes the 
+    // usage of duplicate numbers redundant
+    for(int i = 0; i < arr.size(); i++)
+        if(i == 0 || arr[i] != arr[i-1]) 
+            unique_nums.emplace_back(arr[i]);
+            
+    findCmbSum(0, unique_nums, t, partial, result);
+    return result;
+}
+
+
+// SOLUTION 2
 void findSumCombinations(vector<int>& arr, int k, int curr, int sum, vector<int> partial, 
                         set<vector<int>>& s, vector<vector<int>>& result) {
     
