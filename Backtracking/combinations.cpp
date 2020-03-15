ec83a29d85e5b6fb1732af70b07abf3a1470a8c3
Susanta Biswas <susantasky@gmail.com>
2020-03-15 14:12:59 +0530

    Update combinations.cpp

--- Backtracking/combinations.cpp
+++ Backtracking/combinations.cpp
@@ -1,35 +1,43 @@
 /*
-    Find k sized subsets for numbers from 1 to N
     https://www.interviewbit.com/problems/combinations/
+    
+    TC: O(2^N), worst case: when it exlcudes numbers and reach N
+    
+    The idea is similar to generate Subset problem. Since the range is already known
+    so no need to sort the numbers as well as we will be going from 1, 2....N.
+    
+    Every time two options are there: include or exclude current number.
+    To get the final subsets in sorted order it is important to first do recursion 
+    when the current number is included, this makes sure that smaller gets included first.
+    
+    eg: [1, 2]
+    When included first: [1], [2]
+    [1] is creatd before [2]
 */
 
-void findKSubsets(int n, int k, int curr, vector<int> partial, vector<vector<int>>& result) {
-    // if k sized subset has been created
-    if(curr > n) {
-        // subset is added only if it is k sized
-        if(partial.size() == k) {
-            sort(partial.begin(), partial.end());
-            result.emplace_back(partial);
-        }
+void generateKCmbs(int curr, int n, int k,
+                    vector<int> partial,
+                    vector<vector<int>>& result) {
+    // when k numbers have been traversed
+    if(partial.size() == k || curr > n) {
+        if(partial.size() == k)
+            result.emplace_back(move(partial));
         return;
     }
     
-    // exclude current
-    findKSubsets(n, k, curr + 1, partial, result);
-    
-    // include current number
+    // include current number, to get the smaller number subsets before larger num subsets
     partial.emplace_back(curr);
-    findKSubsets(n, k, curr + 1, partial, result);
+    generateKCmbs(curr + 1, n, k, partial, result);
+    
+    // exclude current number
+    partial.pop_back();
+    generateKCmbs(curr + 1, n, k, partial, result);
 }
-
+                    
 vector<vector<int> > Solution::combine(int n, int k) {
-    vector<vector<int>> result;
     vector<int> partial;
+    vector<vector<int>> result;
     
-    // current is the current number in range [1:n]
-    int curr = 1;
-    findKSubsets(n, k, curr, partial, result);
-    sort(result.begin(), result.end());
-    
+    generateKCmbs(1, n, k, partial, result);
     return result;
 }
