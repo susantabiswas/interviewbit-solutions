5d4533c47e0f067d29543954e589f4a20e5a0bf1
Susanta Biswas <susantasky@gmail.com>
2020-04-03 20:29:23 +0530

    Update remove_duplicates_array2.cpp

--- Two Pointers/remove_duplicates_array2.cpp
+++ Two Pointers/remove_duplicates_array2.cpp
@@ -1,6 +1,39 @@
 /*
 https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array-ii/ 
 */
+/*
+    abstractive text summarization
+    
+    TC: O(N), SC; O(1)
+*/
+int Solution::removeDuplicates(vector<int> &arr) {
+    if(arr.size() <= 1)
+        return 1;
+        
+    // pos to where the last element was written
+    int write_idx = 0;
+    int i = 0;
+    // handle the first element
+    if(arr[0] == arr[1])
+        i += 2, write_idx += 1;
+    else
+        i = 1;
+        
+    while(i < arr.size()) {
+        if(arr[write_idx] != arr[i]) {
+            arr[++write_idx] = arr[i];
+            // if the next element is also same, write that also
+            if(i+1 < arr.size() && arr[i+1] == arr[i]) {
+                arr[++write_idx] = arr[i+1];
+                ++i;
+            }
+        }
+        ++i;
+            
+    }
+    return write_idx + 1;
+}
+
 
 int Solution::removeDuplicates(vector<int> &arr) {
     int i = 1; 
