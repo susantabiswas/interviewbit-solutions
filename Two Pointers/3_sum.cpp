33de6e83875dc3ca81e612d2dece747db941b36b
Susanta Biswas <susantasky@gmail.com>
2020-04-04 19:55:57 +0530

    Update 3_sum.cpp

--- Two Pointers/3_sum.cpp
+++ Two Pointers/3_sum.cpp
@@ -3,6 +3,38 @@
     https://www.interviewbit.com/problems/3-sum/
 */
 
+/*
+    TC: O(nlogn) + O(n^2) ~ O(n^2)
+*/
+int Solution::threeSumClosest(vector<int> &arr, int target) {
+    // sort the array
+    sort(begin(arr), end(arr));
+    int closest_sum = 0, min_diff = numeric_limits<int> :: max();
+    
+    for(int i = 0; i < arr.size(); i++) {
+        // Fix arr[i] as constant in a + b + c = t,
+        // so it becomes finding j and k such that arr[j] + ar[k] = t - arr[i] is smallest
+        // also check for each triplet if it is closest to target
+        int j = i + 1, k = arr.size() - 1;
+        while(j < k) {
+            // sum b + c
+            int sum = arr[j] + arr[k];
+            // check how close a + b + c  is to target
+            if(abs(arr[i] + sum - target) < min_diff) {
+                min_diff = abs(arr[i] + sum - target);
+                closest_sum = arr[i] + arr[j] + arr[k];
+            }
+            
+            if(sum < target - arr[i])
+                ++j;
+            else 
+                --k;
+        }
+    }
+    return closest_sum;
+}
+
+
 // for a given target, finds two elements whose sum is equal to target
 void twoSum(vector<int>& arr, int k, int idx, int& closest_sum, int& closest_diff) {
     int curr_sum = 0;
