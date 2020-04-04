eb72ffe7a55ea996a4360b3d1d127c2267750a03
Susanta Biswas <susantasky@gmail.com>
2020-04-04 10:14:35 +0530

    Update and rename array_3_pointers.cpp to array-3-pointers.cpp

--- Two Pointers/array-3-pointers.cpp
+++ Two Pointers/array-3-pointers.cpp
@@ -2,12 +2,39 @@
     https://www.interviewbit.com/problems/array-3-pointers/
 */
 
+// Using 3 pointers
+// TC: O(min(M, N, K)), M, N, K are array lengths 
+// The diff is min, when the nums are verey close to each other, so make the
+// smallest value array larger by moving it further
+int Solution::minimize(const vector<int> &A, const vector<int> &B, const vector<int> &C) {
+    int a = 0, b = 0, c = 0;
+    int min_diff = numeric_limits<int> :: max();
+    
+    while(a < A.size() && b < B.size() && c < C.size())  {
+        // compute the min diff for current combination
+        int curr_diff = max({abs(A[a] - B[b]), 
+                            abs(B[b] - C[c]),
+                            abs(C[c] - A[a])});
+        min_diff = min(min_diff, curr_diff);
+        // increment the index of array with the smallest element
+        int min_el = 0;
+        min_el = min({A[a], B[b], C[c]});
+        if(min_el == A[a]) ++a;
+        else if(min_el == B[b]) ++b;
+        else if(min_el == C[c]) ++c;
+    }
+    return min_diff;
+}
+
+
 /*
     We keep track of each array. We start with the first element of each.
     Compute the required value, now the required value is max of diff, then min of that.
     So we need to make the diff small, which can be done when both the operands are comparable
     so everytime we pick the smallest out of three and increment position in that array.
     We keep track of min value meanwhile.
+    
+    TC: O(N) + O(N * klogk), where N: min length array out of 3 arrays
 */
 int Solution::minimize(const vector<int> &A, const vector<int> &B, const vector<int> &C) {
     int i = 0, j = 0, k = 0;
