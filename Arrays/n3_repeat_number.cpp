2d882666edf3838a3aa9dec1ee7fdc375882ca89
Susanta Biswas <susantasky@gmail.com>
2020-04-16 13:45:48 +0530

    Update n3_repeat_number.cpp

--- Arrays/n3_repeat_number.cpp
+++ Arrays/n3_repeat_number.cpp
@@ -3,6 +3,82 @@
   https://www.interviewbit.com/problems/n3-repeat-number/
 */
 
+// TC: O(N), SC: O(1)
+int Solution::repeatedNumber(const vector<int> &nums) {
+    // There are 3 equal parts in the array,
+    // if there exists an element that > N/3, that
+    // means if 3 unique elements are removed at a time till
+    // there are no unique elements, we will have the repeatig element
+    // in the end. a a a b b c c 
+    // Also there can be only 2 numbers > N/3,
+    // so we first need to find the two most frequent elements
+    // also if there is a number > N/3, then  
+    
+    // keeps track of 2 out of 3 elements to remove, 
+    // eventually will have the most common elements
+    // (element, frequency)
+    unordered_map<int, int> frequent_elements;
+    
+    for(int i = 0 ; i < nums.size(); i++) {
+        auto it = frequent_elements.find(nums[i]);
+            
+        // if we dont have sufficient elements to remove, add the element
+        if(frequent_elements.size() < 2 && 
+           it == frequent_elements.end()) {
+            frequent_elements[nums[i]] = 1;
+        }
+        else {
+            // now that we have 2 elements, 3 elements
+            // can only be removed if current element is diff from both of them
+            // current element is diff, then remove the 3 elements
+            if(it == frequent_elements.end()) {
+                // reduce the frequency of tracked elements 
+                for(auto el = frequent_elements.begin(); 
+                    el != frequent_elements.end(); ) {
+                    --(el->second);
+                    if(el->second == 0) {
+                        // no need to track
+                        int number = el->first;
+                        ++el;
+                        frequent_elements.erase(number);
+                    }
+                    else 
+                        ++el;
+                }
+            }
+            // current elements is same as one of 2, increase the frequency count
+            else {
+                ++frequent_elements[nums[i]];
+            }
+        }
+    }
+    
+    // confirm the existence of element with freq > N/3
+    int repeating = -1;
+    int frequency = 0;
+    
+    // reset the frequency counts
+    auto it = frequent_elements.begin();
+    while(it != frequent_elements.end()) {
+        it->second = 0;
+        ++it;
+    }
+    
+    for(const int& num: nums) {
+        if(frequent_elements.count(num)) {
+            ++frequent_elements[num];
+        
+            if(frequent_elements[num] > frequency) {
+                frequency = frequent_elements[num];
+                repeating = num;
+            }
+        }
+    }
+    
+    return frequency > (nums.size() / 3) ? repeating : -1;
+}
+
+
 /*
   We remove three distinct elements each time during traversal. The two elements remaining elements are then
   checked if they are the ones repeated > n/3 times.
