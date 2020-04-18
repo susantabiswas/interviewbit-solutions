138586fd04033fe9811c41c8a9b06133a91f969e
Susanta Biswas <susantasky@gmail.com>
2020-04-18 10:28:40 +0530

    Update maximum_unsorted_subarray.cpp

--- Arrays/maximum_unsorted_subarray.cpp
+++ Arrays/maximum_unsorted_subarray.cpp
@@ -2,6 +2,45 @@
   Find the maximum unsorted subarray
   https://www.interviewbit.com/problems/maximum-unsorted-subarray/
 */
+vector<int> Solution::subUnsort(vector<int> &nums) {
+    int first = -1, second = -1;
+    int range_min = INT_MAX, range_max = INT_MIN;
+    
+    for(int i = 0; i < nums.size() - 1; i++) {
+        // find the term which doesnt follow the order
+        if(nums[i] > nums[i+1]) {
+            // if this is the first index that doesnt follow the order
+            if(first == -1)
+                first = i;
+            // update the last term not following the order
+            second = i+1;
+            range_min = min(range_min, nums[i+1]);
+            range_max = max(range_max, nums[i]);
+        }
+    }
+    // now we know the minimum range that needs to sorted,
+    // we check if there is an element > min on left and element < max
+    // on right
+    
+    // we check for the first element which is bigger as
+    // all the numbers after that should be sorted
+    for(int i = 0; i < first; i++) 
+        if(nums[i] > range_min) {
+            first = i;
+            break;
+        }
+            
+    // we check for the last element which is smaller as all the numbers
+    // before it should be sorted
+    for(int i = nums.size() - 1; i > second; i--) 
+        if(nums[i] < range_max) {
+            second = i;
+            break;
+        }
+    
+    return first == -1 ? vector<int>{-1} : vector<int>{first, second};
+}
+
 
 vector<int> Solution::subUnsort(vector<int> &arr) {
     int s = INT_MAX;
