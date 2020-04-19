847d8299018bb1809113a307b37e106c14702ceb
Susanta Biswas <susantasky@gmail.com>
2020-04-19 20:54:59 +0530

    Update kth_row_of_pascals_triangle.cpp

--- Arrays/kth_row_of_pascals_triangle.cpp
+++ Arrays/kth_row_of_pascals_triangle.cpp
@@ -2,7 +2,30 @@
     Generate kth row of pascal triangle
     https://www.interviewbit.com/problems/kth-row-of-pascals-triangle/
 */
+// TC: O(N), SC: O(N)
+vector<int> Solution::getRow(int line) {
+    // the terms of a line are the binomial coeff for nth line
+    // (n, i), 0 <= i <= n-1, 0 <= n <= line-1, here line is already 0-indexed
+    vector<int> result;
+    
+    // first term
+    int term = 1, prev_term = 1;
+    result.emplace_back(term);
+    
+    // line is taken 0-indexed, pos is also 0-indexed
+    // (line, pos) = line!/(line - pos)!pos!
+    // for 3rd line: 2C0, 2C1, 2C2
+    // Also the previous state can be used
+    for(int pos = 1; pos <= line; pos++) {
+        term = prev_term * (line - (pos - 1)) / pos;
+        result.emplace_back(term);
+        prev_term = term;
+    }
+    return result;
+}
 
+
+// TC: O(N), SC: O(N)
 vector<int> Solution::getRow(int k) {
     if(k == 0)
         return vector<int>{1};
