72738695df32a55e3995302dd3f64348cf192cb9
Susanta Biswas <susantasky@gmail.com>
2020-04-23 16:29:21 +0530

    Update search_for_range.cpp

--- Binary search/search_for_range.cpp
+++ Binary search/search_for_range.cpp
@@ -2,6 +2,51 @@
     Search for a target range
     https://www.interviewbit.com/problems/search-for-a-range/
 */
+
+/*
+    Solution 1
+    We use two kinds of binary search, for getting the first and last element.
+    Then perform the two searches on the entire array to get the [first, last]
+    TC: O(logN), SC: O(1)
+*/
+
+// returns the first instance of target
+int leftBSearch(const vector<int>& nums, int left, int right, int target) {
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] < target)
+            left = mid + 1;
+        else    // A[mid] >= target
+            right = mid - 1;
+    }   
+    return nums[right + 1] == target ? right + 1 : -1;
+}
+
+// returns the last instance of target
+int rightBSearch(const vector<int>& nums, int left, int right, int target) {
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] > target)
+            right = mid - 1;
+        else // A[mid] <= target 
+            left = mid + 1;
+    }   
+    return nums[left-1] == target ? left-1 : -1;
+}
+
+                
+vector<int> Solution::searchRange(const vector<int> &nums, int target) {
+    vector<int> pos(2, -1);
+
+    int start = leftBSearch(nums, 0, nums.size()-1, target);
+    int end = rightBSearch(nums, 0, nums.size()-1, target);
+    
+    pos[0] = start == -1 && end == -1 ? -1 : (start == -1 ? end : start);
+    pos[1] = start == -1 && end == -1 ? -1 : (end == -1 ? start : end);
+    return pos;
+}
+
+// Solution 2
 void findRange(const vector<int>& arr, int l, int h, int& k, vector<int>& range,
                 bool left) {
     if(l <= h) {
