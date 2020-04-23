14d767274c2a0e09f455f2e47c93872059f8f454
Susanta Biswas <susantasky@gmail.com>
2020-04-23 20:06:27 +0530

    Update length_of_last_word.cpp

--- Strings/length_of_last_word.cpp
+++ Strings/length_of_last_word.cpp
@@ -4,6 +4,7 @@
     TC: O(n)
 */
 
+// Solution 1
 int Solution::lengthOfLastWord(const string str) {
     int i = str.size() - 1;
     int end_idx = 0;
@@ -19,4 +20,25 @@ int Solution::lengthOfLastWord(const string str) {
         --i;
         
     return end_idx - i;
+}
+
+// Solution 2
+int Solution::lengthOfLastWord(const string str) {
+    int i = 0;
+    int last_len = 0;
+    
+    while(i < str.size()) {
+        // skip the space chars
+        while(i < str.size() && str[i] == ' ')
+            ++i;
+        
+        // next word started
+        if(i < str.size() && str[i] != ' ')
+            last_len = 0;
+            
+        // count the length of current word
+        while(i < str.size() && str[i] != ' ')
+            ++last_len, ++i;
+    }
+    return last_len;
 }
