53508e3e095b454afc395043e45b13eca269b8da
Susanta Biswas <susantasky@gmail.com>
2020-04-25 17:43:43 +0530

    Update excel_column_number.cpp

--- Maths/excel_column_number.cpp
+++ Maths/excel_column_number.cpp
@@ -2,7 +2,17 @@
     https://www.interviewbit.com/problems/excel-column-number/
     
 */
+// Solution 1
+int Solution::titleToNumber(string str) {
+    int col_num = 0;
+    
+    for(const char& c: str) 
+        col_num = col_num*26 + (c - 'A' + 1);
+    
+    return col_num;
+}
 
+// Solution 2
 int Solution::titleToNumber(string str) {
     const int BASE = 26;
     
