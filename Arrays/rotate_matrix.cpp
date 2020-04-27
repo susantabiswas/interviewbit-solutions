c66f5cb1f975c810950d1a3df4a6feb9a788658c
Susanta Biswas <susantasky@gmail.com>
2020-04-27 08:11:38 +0530

    Update rotate_matrix.cpp

--- Arrays/rotate_matrix.cpp
+++ Arrays/rotate_matrix.cpp
@@ -3,6 +3,7 @@
     https://www.interviewbit.com/problems/rotate-matrix/
 */
 
+// Solution 1
 void Solution::rotate(vector<vector<int> > &mat) {
     int row_s = 0, col_s = 0, 
         row_e = mat.size() - 1, col_e = mat[0].size() - 1;
@@ -28,4 +29,31 @@ void Solution::rotate(vector<vector<int> > &mat) {
             ++row_s, --row_e, ++col_s, --col_e;
         
     }
+}
+
+// Solution 2
+void Solution::rotate(vector<vector<int> > &grid) {
+    const int N = grid.size();
+    int row_start = 0, row_end = N-1, col_start = 0, col_end = N-1;
+    
+    // since row and cols are N, we can only include one boundary cond
+    while(row_start < row_end) {
+        // start swapping the outer boundary elements
+        int num_elements = col_end - col_start;
+        for(int i = 0; i < num_elements; i++) {
+            int temp = grid[row_start][col_start + i];
+            // 1st row and last column
+            swap(temp, grid[row_start + i][col_end]);
+        
+            // last column and last row
+            swap(temp, grid[row_end][col_end - i]);
+            
+            // last row and 1st column
+            swap(temp, grid[row_end - i][col_start]);
+             
+            // 1st column and 1st row
+            swap(temp, grid[row_start][col_start + i]);
+        }
+        ++row_start, --row_end, ++col_start, --col_end;
+    }    
 }
