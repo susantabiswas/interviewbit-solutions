a909c902a7028187c0b6ad08e2511b0bb7ad0c2c
Susanta Biswas <susantasky@gmail.com>
2020-04-28 10:28:40 +0530

    Update colorful_number.cpp

--- Hashing/colorful_number.cpp
+++ Hashing/colorful_number.cpp
@@ -1,7 +1,30 @@
 /*
     https://www.interviewbit.com/courses/programming/topics/hashing/
 */
+// Solution 1
+// TC: O(d^2), d: no. of digits in number
+// or TC: O(log10(n) ^ 2)
+int Solution::colorful(int n) {
+    unordered_set<int> product;
+    string num = to_string(n);
+    
+    for(int i = 0; i < num.size(); i++) {
+        long long prod = 1;
+        for(int j = i; j < num.size(); j++) {
+            // find the product of current subsequence
+            prod = prod * (num[j] - '0');
+            // check if the same product existed before or not
+            if(product.find(prod) != product.end())
+                return 0;
+            // insert the product in hash table
+            product.emplace(prod);
+        }
+    }
+    return 1;
+}
 
+
+//Solution 2
 int Solution::colorful(int num) {
     vector<int> digits;
     while(num) {
