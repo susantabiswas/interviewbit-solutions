c10b77f3a8aabfaa886e595aee03b2756dcbf22e
Susanta Biswas <susantasky@gmail.com>
2020-05-03 13:08:54 +0530

    Update add_binary_strings.cpp

--- Strings/add_binary_strings.cpp
+++ Strings/add_binary_strings.cpp
@@ -4,8 +4,42 @@
 
     TC: O(n)
 */
+// Solution 1
+string Solution::addBinary(string num1, string num2) {
+    int carry = 0;
+    int i = num1.size() - 1, j = num2.size() - 1;
+    string result;
+    
+    while(i >= 0 && j >= 0) {
+        int sum = (num1[i] - '0') + (num2[j] - '0') + carry;
+        result += to_string(sum % 2 != 0 ? 1 : 0);
+        carry = sum > 1;
+        --i, --j;
+    }
+    // one of the numbers are remaining
+    while(i >= 0) {
+        int digit = (num1[i] - '0') + carry;
+        result += to_string(digit % 2 != 0 ? 1 : 0);
+        carry = digit > 1;
+        --i;
+    }
+    while(j >= 0) {
+        int sum = (num2[j] - '0') + carry;
+        result += to_string(sum % 2 != 0 ? 1 : 0);
+        carry = sum > 1;
+        --j;
+    }
+    // carry is remaining
+    if(carry)
+        result += to_string(carry);
+    
+    // reverse the string
+    reverse(result.begin(), result.end());
+    return result;
+}
 
-
+//////////////////////////
+// Solution 2
 string Solution::addBinary(string num1, string num2) {
     string result;
     int carry = 0;
