0c47d65d72773d89853b5b3f4b104102c7c4541e
Susanta Biswas <susantasky@gmail.com>
2020-05-04 20:32:31 +0530

    Update permutations.cpp

--- Backtracking/permutations.cpp
+++ Backtracking/permutations.cpp
@@ -3,6 +3,49 @@
   https://www.interviewbit.com/problems/permutations/
 */
 
+// Solution 1
+//TC: O(N^2)
+bool nextPermutation(vector<int> &arr) {
+    // check if in decreasing order
+    // find the first number pair from right, that violates the order
+    int i = arr.size() - 2;
+    while(i >= 0 && arr[i] >= arr[i+1])
+        --i;
+    // if array is in decreasing order
+    if(i == -1) {
+        return false;
+    }
+    
+    // next permutation is possible
+    // find the first number which is greater than i-th number
+    int j = arr.size() - 1;
+    while(j > i && arr[j] <= arr[i])
+        --j;
+    // [i:j] can be rearranged for generating a bigger number
+    // swap the numbers, all the numbers in [i+1:] needs to be reversed
+    // so that [i:] makes the smallest arrangement
+    swap(arr[i], arr[j]);
+    reverse(arr.begin() + i + 1, arr.end());
+    return true;
+}
+
+
+vector<vector<int> > Solution::permute(vector<int> &arr) {
+    vector<vector<int>> result;
+    
+    // sort the array
+    sort(begin(arr), end(arr));
+    result.emplace_back(arr);
+    while(nextPermutation(arr)) {
+        result.emplace_back(arr);
+    }
+    
+    return result;
+}
+
+////////////////////////////////////////////////////////
+// Solution 2
+// TC: O(2^N)
 void permutateElements(vector<int>& arr, int curr, vector<int> partial,
                             vector<vector<int>>& result) {
     // all the elements have been arranged
